1d3014bfe283a9e6b7cac589ff8cab13117d1bbc
JJ <jj.spetseris@yahoo.com>
2017-09-04 16:42:40 -0500

    added download feature

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,5 +1,6 @@
 import {Component, Output, EventEmitter} from '@angular/core';
 import {SharedService} from '../shared.service';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'input-root',
@@ -20,5 +21,19 @@ export class InputComponent{
   inputChange(event: any) {
       this.data = event.target.value;
       this.change.emit(this.data);
+  }
+
+  download() {
+    let name = (<HTMLInputElement>document.getElementById("fileName")).value;
+    let blob = new Blob([this.data], {
+              type: "text/plain;charset=utf-8"
+          });
+
+    if (name) {
+      name = name + ".md";
+    } else {
+      name = "README.md";
+    }
+    FileSaver.saveAs(blob, name);
   }
 }
